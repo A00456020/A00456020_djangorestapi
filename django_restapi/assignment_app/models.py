01ac6e89561334acd27d605340c8656202186951
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-04 19:33:01 -0400

    created hotel model and applied migration to database

--- django_restapi/assignment_app/models.py
+++ django_restapi/assignment_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Hotels(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=False)
+    total_number_of_rooms = models.IntegerField()
+    number_of_rooms_available = models.IntegerField()
+    website_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
