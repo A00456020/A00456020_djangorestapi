253beba50c445181809a824062af02e6d182a284
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-04 18:56:36 -0400

    updated mysql database details in settings.py

--- django_restapi/django_restapi/settings.py
+++ django_restapi/django_restapi/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_restapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangorestapi_db',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'djangodbadmin',
+        'PASSWORD': 'access',
     }
 }
 
